98b0d418e49c02e7fa0b2b8c421a335580f07680
felicetto13 <felice.laterza93@icloud.com>
2022-06-09 13:19:53 +0200

    bonus part completed

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 const app = new Vue({
     el: '#app',
     data: {
-        textH1: 'Questa è la mia prima prova in Vue! Sono molto Felice!'
+        textH1: 'Questa è la mia prima prova in Vue! Sono molto Felice!',
+        srcAddress: 'https://picsum.photos/1200/900'
     }
 })
